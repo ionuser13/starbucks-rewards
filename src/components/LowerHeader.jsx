2574ddfd79123c834d5bd9a30d8b2e1713e2a7a2
John Chacpi <johnchacpi479@gmail.com>
2023-01-31 17:03:31 -0500

    am created sticky header

---
+++ src/components/LowerHeader.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LowerHeader = () => {
+    return (
+        <div>
+            <h1>Starbucks® Rewards</h1>
+            <a href="/">Join in the app</a>
+        </div>
+    )
+}
+
+export default LowerHeader
