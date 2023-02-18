cf2dd2e498e0e84c7c1d977a6ddd9a761ee56eea
John Chacpi <johnchacpi479@gmail.com>
2023-02-17 22:42:06 -0500

    progress with main desktop

--- src/components/MainDesktopContent.jsx
+++ src/components/MainDesktopContent.jsx
@@ -3,14 +3,14 @@ import JoinButton from './JoinButton';
 
 const MainDesktopContent = () => {
     return (
-        <div className='h-full'>
+        <div className='w-full h-full flex flex-col justify-start items-center'>
             <h2 className='text-4xl font-semibold leading-tight'>FREE COFFEE <br /> IS A TAP AWAY </h2>
             <div>
-                <p className='text-[19px]'>Join now to start earning Rewards.</p>
-                <div className='buttonContainer'>
+                <p className='text-[19px] mt-[1.6rem]'>Join now to start earning Rewards.</p>
+                <div className='buttonContainer mt-[3.2rem]'>
                     <JoinButton innerText={"Join now"} bgColor={"#008248"} borColor={"#008248"}/>   
                 </div>
-                <div className='text-[1.9rem]'>
+                <div className='text-[1.9rem] mt-[1.6rem]'>
                     <p>Or
                         <a href='/'> join the app </a>
                     for the best experience</p>
--- src/containers/MainDesktop.jsx
+++ src/containers/MainDesktop.jsx
@@ -3,7 +3,7 @@ import MainDesktopContent from '../components/MainDesktopContent';
 
 const MainDesktop = () => {
     return (
-        <div className='bg-desktop bg-light-green bg-[50%] bg-[length:auto_100%] bg-no-repeat w-full h-[610px]'>
+        <div className='bg-desktop bg-light-green bg-[50%] bg-[length:auto_100%] bg-no-repeat w-full flex'>
             <MainDesktopContent />
         </div>
     )
