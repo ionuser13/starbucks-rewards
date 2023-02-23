7406bb9db8fba1fc49fd26f34c507de939c35428
John Chacpi <johnchacpi479@gmail.com>
2023-02-22 19:21:49 -0500

    star components

---
+++ src/components/StarTermsComponents/EarningStars.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const EarningStars = () => {
+    return (
+        <div className='w-[50%] pb-[3.2rem] inline-block relative] text-left align-top'>
+
+        </div>
+    )
+}
+
+export default EarningStars;
---
+++ src/components/StarTermsComponents/Restrictions.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Restrictions = () => {
+    return <p className='text-[1.4rem] pb-[3.2rem]'>At participating stores. Restrictions apply.</p>
+};
+
+export default Restrictions
