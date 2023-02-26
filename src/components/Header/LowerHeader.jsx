5792d123d63dab9b9ec0cd31d094a07e27ccb5f3
John Chacpi <johnchacpi479@gmail.com>
2023-02-26 13:24:29 -0500

    changed icons to assets, changed to mobile first the lower header

--- src/components/Header/LowerHeader.jsx
+++ src/components/Header/LowerHeader.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import JoinButton from './JoinButton';
 import "../../styles/lowerHeader.css"
 
 const LowerHeader = () => {
     return (
-        <div className='lowerHeader flex items-center bg-dark-green h-[50px] sticky top-[-1px] z-[3]'>
-            <h1 className='tracking-widest uppercase pl-[131px] font-bold text-white text-sm'>Starbucks® Rewards</h1>
-            <a className='hidden' href="/">Join in the app</a>
+        <div className='lowerHeader bg-dark-green w-full h-[50px] sticky top-[-1px] z-[3]'>
+            <div className='flex items-center justify-between h-full px-[1.6rem] md:px-[2.4rem] md:pl-[99px] lg:px-[4rem] lg:pl-[131px] min-[1520px]:mx-auto min-[1520px]:max-w-[1440px] min-[1520px]:px-0  min-[1600px]:pl-0'>
+                <h1 className='tracking-widest uppercase font-bold text-white text-[1.3rem] md:text-[1.4rem]'>Starbucks® Rewards</h1>
+                <div className='md:hidden shrink-0 whitespace-nowrap'>
+                    <JoinButton innerText={"Join in the app"} bgColor={"transparent"} borColor={"white"}/>
+                </div>
+            </div>
         </div>
     )
 }
