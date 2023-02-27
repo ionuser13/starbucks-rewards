8d7c45a064faac7797565cb13b256b7a13a2104c
John Chacpi <johnchacpi479@gmail.com>
2023-02-27 09:46:57 -0500

    header logo fix

--- src/containers/Header.jsx
+++ src/containers/Header.jsx
@@ -9,9 +9,9 @@ import "../styles/upperHeader.css";
 const Header = () => {
     return (
         <header className='relative z-[2] upperHeader'>
-            <div className='px-[1.6rem] md:px-[2.4rem] lg:px-[4rem] min-[1520px]:mx-[auto] min-[1520px]:max-w-[1440px] min-[1520px]:px-[0]'>
+            <div className='px-[1.6rem] md:px-[2.4rem] lg:px-[4rem] min-[1520px]:mx-[auto] min-[1520px]:max-w-[1440px] min-[1520px]:px-[0] min-[1520px]:relative'>
                 <div className='flex items-center'>
-                    <div className="mr-[1.6rem] my-[1.6rem] md:my-0 md:mr-[2.4rem] lg:mr-[4rem] w-[32px] h-[32px] min-[375px]:w-[40px] min-[375px]:h-[40px] md:w-[51px] md:h-[51px]">
+                    <div className="mr-[1.6rem] my-[1.6rem] md:my-0 md:mr-[2.4rem] lg:mr-[4rem] w-[32px] h-[32px] min-[375px]:w-[40px] min-[375px]:h-[40px] md:w-[51px] md:h-[51px] min-[1702px]:absolute min-[1702px]:right-[100%] min-[1702px]:top-[50%] min-[1702px]:translate-y-[-50%]">
                         <StarLogo />
                     </div>
                     <div className='hidden md:flex md:items-center grow'>
