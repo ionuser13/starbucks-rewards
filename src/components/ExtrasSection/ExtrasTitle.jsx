09b25678cd14e7da0f73056a30b73b0a89796cd4
John Chacpi <johnchacpi479@gmail.com>
2023-02-27 13:16:48 -0500

    endless extras modified to first mobile

--- src/components/ExtrasSection/ExtrasTitle.jsx
+++ src/components/ExtrasSection/ExtrasTitle.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const ExtrasTitle = () => {
     return (
-        <div className='w-[50%] mx-auto'>
-            <h2 className='text-[28px] font-semibold'>Endless Extras</h2>
-            <p className='text-[16px] pt-[16px]'>Joining Starbucks® Rewards means unlocking access to exclusive benefits. Say hello to easy ordering, tasty Rewards and &#8212; yes, free coffee.</p>
+        <div className='text-center w-full md:ml-[16.667%] md:w-[66.667%] lg:ml-[25%] lg:w-[50%]'>
+            <h2 className='text-[2rem] min-[375px]:text-[2.4rem] md:text-[2.8rem] font-semibold'>Endless Extras</h2>
+            <p className='text-center text-[16px] pt-[16px]'>Joining Starbucks® Rewards means unlocking access to exclusive benefits. Say hello to easy ordering, tasty Rewards and &#8212; yes, free coffee.</p>
         </div>
     )
 }
