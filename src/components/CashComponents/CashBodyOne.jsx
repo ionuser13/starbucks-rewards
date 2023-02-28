c6523f5b695252c701090c2f69da7ed202238853
John Chacpi <johnchacpi479@gmail.com>
2023-02-28 11:13:13 -0500

    changed cash section to mobile first

--- src/components/CashComponents/CashBodyOne.jsx
+++ src/components/CashComponents/CashBodyOne.jsx
@@ -3,29 +3,29 @@ import React from 'react';
 const CashBodyOne = () => {
     return (
         <div className='OneDollar block m-0 p-0 text-[16px] text-left'>
-            <div className='inline-block m-0 p-0 relative text-left align-top w-full'>
-                <p className='font-semibold text-[24px]'>1 <span aria-hidden="true" class="color-black text-sm">★</span> per dollar</p>
+            <div className='inline-block m-0 pb-[3.2rem] relative text-left align-top w-full lg:p-0 lg:w-[16.667%]'>
+                <p className='font-semibold text-[1.9rem] md:text-[2.4rem]'>1 <span aria-hidden="true" class="color-black text-sm">★</span> per dollar</p>
                 <p>Pay as you go</p>
             </div>
-            <div className='inline-block w-[33.33%] relative text-left align-top px-[24px]'>
-                <div className='flex gap-[16px] scan'>
-                    <div className='flex max-w-[128px] shrink-0 relative text-left align-top'>
-                        <img src="https://www.starbucks.com/weblx/images/rewards/ways-to-pay/1A.png" alt="Scan and pay separately" className='w-full h-auto' width={112}/>
+            <div className='inline-block m-0 pb-[3.2rem] relative text-left align-top w-full md:p-0 md:w-[50%] lg:w-[41.667%] xl:w-[33.33%]'>
+                <div className='flex md:pr-[0.8rem] lg:px-[2.4rem]'>
+                    <div className='text-center shrink-0 m-0 pr-[1.6rem] max-w-[128px]'>
+                        <img src="https://www.starbucks.com/weblx/images/rewards/ways-to-pay/1A.png" alt="Scan and pay separately" className='max-w-full h-auto fading' width={112}/>
                     </div>
-                    <div>
-                        <h3 className='text-[19px] font-semibold'>Scan and pay separately</h3>
-                        <p className='pt-[16px]'>Use cash or credit/debit card at the register.</p>
+                    <div className='text-left min-w-[calc(100%-128px)]'> 
+                        <h3 className='text-[1.6rem] font-semibold md:text-[1.9rem]'>Scan and pay separately</h3>
+                        <p className='pt-[1.6rem]'>Use cash or credit/debit card at the register.</p>
                     </div>
                 </div>
             </div>
-            <div className='inline-block w-[33.33%] relative text-left align-top px-[24px]'>
-                <div className='save flex gap-[16px]'>
-                    <div className='flex max-w-[128px] shrink-0'>
-                        <img src="https://www.starbucks.com/weblx/images/rewards/ways-to-pay/1B.png" alt="Save payment in the app" className='w-full h-auto' width={112} />
+            <div className='inline-block m-0 pb-[3.2rem] relative text-left align-top w-full md:p-0 md:w-[50%] lg:w-[41.667%] xl:w-[33.33%]'>
+                <div className='flex md:pr-[0.8rem] lg:px-[2.4rem]'>
+                    <div className='text-center shrink-0 m-0 pr-[1.6rem] max-w-[128px]'>
+                        <img src="https://www.starbucks.com/weblx/images/rewards/ways-to-pay/1B.png" alt="Save payment in the app" className='max-w-full h-auto fading' width={112} />
                     </div>
-                    <div>
-                        <h3 className='text-[19px] font-semibold'>Save payment in the app</h3>
-                        <p className='pt-[16px]'>Check-out faster by saving a credit/debit card or PayPal to your account. You&#8217;ll be able to order ahead or scan and pay at the register in one step.</p>
+                    <div className='text-left min-w-[calc(100%-128px)]'>
+                        <h3 className='text-[1.6rem] font-semibold md:text-[1.9rem]'>Save payment in the app</h3>
+                        <p className='pt-[1.6rem]'>Check-out faster by saving a credit/debit card or PayPal to your account. You&#8217;ll be able to order ahead or scan and pay at the register in one step.</p>
                     </div>
                 </div>
             </div>
--- src/components/CashComponents/CashBodyTwo.jsx
+++ src/components/CashComponents/CashBodyTwo.jsx
@@ -2,32 +2,30 @@ import React from 'react';
 
 const CashBodyTwo = () => {
     return (
-        <div className='supremeCashContainer text-[16px] text-left'>
-            <div className='TwoDollar block'>
-                <div className='inline-block w-[16.667%] relative text-left align-top'>
-                    <p className='font-semibold text-[24px]'>2 <span aria-hidden="true" class="color-black text-sm">★</span> per dollar</p>
-                    <p>Add funds in the app</p>
-                </div>
-                <div className='inline-block w-[33.33%] relative text-left align-top px-[24px]'>
-                    <div className='flex gap-[16px] scan'>
-                        <div className='flex max-w-[128px] shrink-0 relative text-left align-top'>
-                            <img src="https://www.starbucks.com/weblx/images/rewards/ways-to-pay/2A.png" alt="Preload" className='w-full h-auto' width={112}/>
-                        </div>
-                        <div>
-                            <h3 className='text-[19px] font-semibold'>Preload</h3>
-                            <p className='pt-[16px]'>To save time and earn Stars twice as fast, add money to your digital Starbucks Card using any payment option. Scan and pay in one step or order ahead in the app.</p>
-                        </div>
+        <div className='TwoDollar block m-0 p-0 text-[16px] text-left'>
+            <div className='inline-block m-0 pb-[3.2rem] relative text-left align-top w-full lg:p-0 lg:w-[16.667%]'>
+                <p className='font-semibold text-[1.9rem] md:text-[2.4rem]'>2 <span aria-hidden="true" class="color-black text-sm">★</span> per dollar</p>
+                <p>Add funds in the app</p>
+            </div>
+            <div className='inline-block m-0 pb-[3.2rem] relative text-left align-top w-full md:p-0 md:w-[50%] lg:w-[41.667%] xl:w-[33.33%]'>
+                <div className='flex md:pr-[0.8rem] lg:px-[2.4rem]'>
+                    <div className='text-center shrink-0 m-0 pr-[1.6rem] max-w-[128px]'>
+                        <img src="https://www.starbucks.com/weblx/images/rewards/ways-to-pay/2A.png" alt="Preload" className='max-w-full h-auto fading' width={112}/>
+                    </div>
+                    <div className='text-left min-w-[calc(100%-128px)]'>
+                        <h3 className='text-[1.6rem] font-semibold md:text-[1.9rem]'>Preload</h3>
+                        <p className='pt-[1.6rem]'>To save time and earn Stars twice as fast, add money to your digital Starbucks Card using any payment option. Scan and pay in one step or order ahead in the app.</p>
                     </div>
                 </div>
-                <div className='inline-block w-[33.33%] relative text-left align-top px-[24px]'>
-                    <div className='save flex gap-[16px]'>
-                        <div className='flex max-w-[128px] shrink-0'>
-                            <img src="https://www.starbucks.com/weblx/images/rewards/ways-to-pay/2B.png" alt="Register your gift card" className='w-full h-auto' width={112} />
-                        </div>
-                        <div>
-                            <h3 className='text-[19px] font-semibold'>Register your gift card</h3>
-                            <p className='pt-[16px]'>Then use it to pay through the app. You can even consolidate balances from multiple cards in one place.</p>
-                        </div>
+            </div>
+            <div className='inline-block m-0 pb-[3.2rem] relative text-left align-top w-full md:p-0 md:w-[50%] lg:w-[41.667%] xl:w-[33.33%]'>
+                <div className='flex md:pr-[0.8rem] lg:px-[2.4rem]'>
+                    <div className='text-center shrink-0 m-0 pr-[1.6rem] max-w-[128px]'>
+                        <img src="https://www.starbucks.com/weblx/images/rewards/ways-to-pay/2B.png" alt="Register your gift card" className='max-w-full h-auto fading' width={112} />
+                    </div>
+                    <div className='text-left min-w-[calc(100%-128px)]'>
+                        <h3 className='text-[1.6rem] font-semibold md:text-[1.9rem]'>Register your gift card</h3>
+                        <p className='pt-[1.6rem]'>Then use it to pay through the app. You can even consolidate balances from multiple cards in one place.</p>
                     </div>
                 </div>
             </div>
